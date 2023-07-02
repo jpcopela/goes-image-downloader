ee90c1cc54c3af531ea10e1ad26e1d3878dea9c7
jackcop <copeland.jackson13@gmail.com>
2023-07-02 18:14:07 -0500

    fixed directory name

--- publish-images.sh
+++ publish-images.sh
@@ -9,24 +9,24 @@ then
 	echo "Updating Earthview..."
 
 	#rename old image and update background path
-	mv /home/jackson/Projects/goes_image_downloader/latest.jpg /home/jackson/Projects/goes_image_downloader/former.jpg
+	mv /home/jackson/Projects/goes-image-downloader/latest.jpg /home/jackson/Projects/goes-image-downloader/former.jpg
 
-	gsettings set org.gnome.desktop.background picture-uri file:////home/jackson/Projects/goes_image_downloader/former.jpg
+	gsettings set org.gnome.desktop.background picture-uri file:////home/jackson/Projects/goes-image-downloader/former.jpg
 
 	#download image from https://www.star.nesdis.noaa.gov/GOES/fulldisk_band.php?sat=G16&band=GEOCOLOR&length=12
-	wget -nd -A.jpg https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/latest.jpg -O /home/jackson/Projects/goes_image_downloader/latest_unformatted.jpg
+	wget -nd -A.jpg https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/latest.jpg -O /home/jackson/Projects/goes-image-downloader/latest_unformatted.jpg
 
 	#resize new image
-	convert /home/jackson/Projects/goes_image_downloader/latest_unformatted.jpg -resize 1440x900 -background black -gravity center -extent 1440x900 /home/jackson/Projects/goes_image_downloader/latest.jpg
+	convert /home/jackson/Projects/goes-image-downloader/latest_unformatted.jpg -resize 1920x1080 -background black -gravity center -extent 1920x1080 /home/jackson/Projects/goes-image-downloader/latest.jpg
 
 	#delete unformatted image
-	rm /home/jackson/Projects/goes_image_downloader/latest_unformatted.jpg
+	rm /home/jackson/Projects/goes-image-downloader/latest_unformatted.jpg
 
 	#set new image as background
-	gsettings set org.gnome.desktop.background picture-uri file:////home/jackson/Projects/goes_image_downloader/latest.jpg
+	gsettings set org.gnome.desktop.background picture-uri file:////home/jackson/Projects/goes-image-downloader/latest.jpg
 
 	#delete former image
-	rm /home/jackson/Projects/goes_image_downloader/former.jpg
+	rm /home/jackson/Projects/goes-image-downloader/former.jpg
 
 else
 	echo "No internet connection..."
